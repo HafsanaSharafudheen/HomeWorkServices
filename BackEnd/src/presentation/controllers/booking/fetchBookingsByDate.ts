bbe60767c35ee6b4c0501705faeaa5605dcf0b37
HafsanaSharafudheen <111694967+HafsanaSharafudheen@users.noreply.github.com>
2025-01-30 20:45:26 +0530

    some consoleds are added to debug

--- BackEnd/src/presentation/controllers/booking/fetchBookingsByDate.ts
+++ BackEnd/src/presentation/controllers/booking/fetchBookingsByDate.ts
@@ -4,13 +4,14 @@ import { getUserBookingsBySelectedTime } from "../../../application/businesslogi
 export const getUserBookingsbyTime = async (req:any, res:any) => {
     try {
       const { providerId, selectedTime, selectedDate } = req.query;
-  
+  console.log("entered insid ethe getuserBookingsByTIme")
       if (!providerId) {
         return res.status(400).json({ message: "providerId is required." });
       }
   
       const bookings = await getUserBookingsBySelectedTime(providerId, selectedTime, selectedDate);
-  
+      console.log("bookings in getuserBookingsByTIme",bookings)
+
       res.status(200).json({ bookings });
     } catch (error) {
       console.error("Error in getUserBookingsbyTime:", error);
